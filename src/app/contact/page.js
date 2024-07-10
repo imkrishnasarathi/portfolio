0b15d76cdc9c894fc0ae51e4dc5f21da62278d8c
imkrishnasarathi <krishnasarathishem@gmail.com>
2024-07-10 23:18:13 +0530

    add files to repo

---
+++ src/app/contact/page.js
@@ -0,0 +1,10 @@
+const page = () => {
+  return (
+    <div>
+      RANDOM CONTENT
+    </div>
+  )
+}
+
+export default page
+
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,8 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "./components/Navbar";
+import { SidebarItem } from "./components/Navbar";
+import { House, CircleUser, FolderOpenDot, Mail, Newspaper } from "lucide-react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +14,16 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+      <Navbar>
+        <SidebarItem icon={<House />} text="Home" isActive={true} pageHref="/" />
+        <SidebarItem icon={<CircleUser />} text="About" pageHref="/about" />
+        <SidebarItem icon={<FolderOpenDot />} text="Projects" pageHref="/projects" />
+        <SidebarItem icon={<Newspaper />} text="Articles" pageHref="/articles" />
+        <SidebarItem icon={<Mail />} text="Contact" pageHref="/contact"/>
+      </Navbar>
+      {children}
+      </body>
     </html>
   );
 }
