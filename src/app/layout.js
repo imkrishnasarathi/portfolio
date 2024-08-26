1aed72f3492c04d60ca23237cf41dd25e7297e7d
imkrishnasarathi <krishnasarathishem@gmail.com>
2024-08-26 15:31:17 +0530

    update layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,6 @@
 import { Poppins } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
-import { SidebarItem } from "./components/Navbar";
 import { House, FolderOpenDot, Mail, Newspaper } from "lucide-react";
 import Footer from "./components/Footer.jsx";
 import Head from 'next/head';
@@ -17,15 +16,11 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <Head>
   			<link rel='icon' href='/pic.png' />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
   		</Head>
       <body className={`${poppins.className} bg-[#07142b] text-[#dcdcde]`}>
-        <Navbar>
-          <SidebarItem icon={<House />} text="Home" isActive={true} pageHref="/" />
-          <SidebarItem icon={<FolderOpenDot />} text="Projects" pageHref="/projects" />
-          <SidebarItem icon={<Newspaper />} text="Articles" pageHref="/articles" />
-          <SidebarItem icon={<Mail />} text="Contact" pageHref="/contact" />
-        </Navbar>
-        <div className="ml-20 flex flex-col min-h-screen overflow-hidden">
+        <Navbar/>
+        <div className="flex flex-col min-h-screen overflow-hidden mt-16">
           <main className="flex flex-grow pl-5 pt-10 flex-col pr-5">{children}</main>
           <Footer />
         </div>
