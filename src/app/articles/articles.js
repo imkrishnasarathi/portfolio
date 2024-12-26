7f5e167ca87d8414a1a902bd0e67a47e02a0a863
imkrishnasarathi <krishnasarathishem@gmail.com>
2024-12-26 21:20:31 +0530

    update articles.js

---
+++ src/app/articles/articles.js
@@ -0,0 +1,20 @@
+const articles = [
+    {
+        title: "How to Run Open Source LLMs on Your Own Computer Using Ollama",
+        coverImg: "https://cdn.hashnode.com/res/hashnode/image/upload/v1734681473969/20c1a1cd-898a-4f48-a26f-d2d3d2917efc.png"
+    },
+    {
+        title: "From Fears to Achievements: My Tech Journey and the Lessons Learned Along the Way",
+        coverImg: "https://pbs.twimg.com/media/FOEtHowXsAQwRhZ?format=jpg&name=900x900"
+    },
+    {
+        title: "Understanding the difference between Static Site Generation (SSG) and Server Side Rendering (SSR)",
+        coverImg: "https://media2.dev.to/dynamic/image/width=1000,height=420,fit=cover,gravity=auto,format=auto/https%3A%2F%2Fdev-to-uploads.s3.amazonaws.com%2Fuploads%2Farticles%2Fgbsj22505e9smoku8irh.jpeg",
+    },
+    {
+        title: "Discover Wit.ai: Create Your Own Intelligent Bots for Free 🚀🤖",
+        coverImg: "https://media2.dev.to/dynamic/image/width=1000,height=420,fit=cover,gravity=auto,format=auto/https%3A%2F%2Fdev-to-uploads.s3.amazonaws.com%2Fuploads%2Farticles%2Fe14a38lxzj99egcmesn5.jpeg",
+    },
+];
+
+export default articles;
